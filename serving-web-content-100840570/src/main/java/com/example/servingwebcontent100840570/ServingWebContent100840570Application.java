c3c0270c468d907c9c108ed869b30fe04ca09401
Sean <Sean@LAPTOP-1JPQ3J7C>
2023-12-13 15:18:26 -0500

    Initial Commit For Lab

---
+++ serving-web-content-100840570/src/main/java/com/example/servingwebcontent100840570/ServingWebContent100840570Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100840570;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100840570Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100840570Application.class, args);
+	}
+
+}
---
+++ serving-web-content-100840570/src/test/java/com/example/servingwebcontent100840570/ServingWebContent100840570ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100840570;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContent100840570ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
